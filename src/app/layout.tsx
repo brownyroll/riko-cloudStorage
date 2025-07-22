28245d4764dfea216fcb21484b7c38b957d240bb
Bamz Kittisak Udomsri <76609512+brownyroll@users.noreply.github.com>
2025-07-22 12:22:18 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
